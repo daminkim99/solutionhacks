eda939cb7f5409106a320a4b784d9de062f5e6a8
Damin (Elena) Kim <elenanana0903@gmail.com>
2025-06-28 03:38:58 -0400

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 import pymongo 
 from pymongo import MongoClient
 import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 #getting the environment variable for MongoDB password
+
 MONGO_PASS = os.environ.get("MONGO_PASS")  # Safely get the password from env
 if not MONGO_PASS:
     raise ValueError("MONGOPASS environment variable is not set.")
@@ -11,9 +15,13 @@ if not MONGO_PASS:
 uri = f"mongodb+srv://xenap26:{MONGO_PASS}@cluster0.dsiruap.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 cluster = MongoClient(uri)
 
-
+#Testing the connection
 db = cluster["UserData"]
 collection = db["flask_mongo"]
-
 collection.insert_one({"_id":3, "user_name":"Elena"})
 collection.insert_one({"_id":100, "user_name":"Xena"})
+
+#routes 
+@app.route("/")
+def home():
+    return "MongoDB + Flask API is running!"
