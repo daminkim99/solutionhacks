35fb0fc0b38fbefd38f2a362e7c1e45ae8f56840
Damin (Elena) Kim <elenanana0903@gmail.com>
2025-06-29 01:56:16 -0400

    change to the add.jsx

--- frontend/src/views/add.jsx
+++ frontend/src/views/add.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import '../App.css';
 
-const Add = () => {
+export function Add() {
   const [formData, setFormData] = useState({
     name: '',
     address: '',
