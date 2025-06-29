b95556afa8a0a90b5f01faa1d88e160b689c9f11
Damin (Elena) Kim <elenanana0903@gmail.com>
2025-06-29 01:51:13 -0400

    merge conflicts resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,3 @@
-// import './App.css';
-// import { Login } from './views/signup'; // adjust the path if needed
-
-// function App() {
-//   return (
-//     <Login />
-//   );
-// }
-
-// export default App;
-
-
 
 
 import './App.css';
